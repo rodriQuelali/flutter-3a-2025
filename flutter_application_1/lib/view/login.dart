47bac09782818462312e007bb54bea784d7d4b75
rodriquelali <rodriquelali@gmail.com>
2025-06-18 19:42:44 -0400

    New peoywct Calculadora.

--- flutter_application_1/lib/view/login.dart
+++ flutter_application_1/lib/view/login.dart
@@ -1,4 +1,4 @@
-import 'dart:nativewrappers/_internal/vm/lib/math_patch.dart';
+
 
 import 'package:flutter/material.dart';
 
@@ -13,16 +13,19 @@ class _LoginPagesState extends State<LoginPages> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Icon(Icons.person, color: Color.fromRGBO(4, 42, 109, 1),),
         title: Text("Login 3ro A", style: TextStyle(color: Color.fromRGBO(4, 42, 109, 1)),),
-        backgroundColor: Color.fromRGBO(248, 160, 160, 1),
+        backgroundColor: Color.fromRGBO(251, 246, 246, 1),
       ),
       body: Container(
+        color: Color.fromRGBO(237, 235, 235, 1),
+        padding: EdgeInsets.all(20),
         child: Column(
           children: [
             titulo(),
             SizedBox(height: 20,),
             campoTexto("Usuario", Icons.person),
-            SizedBox(height: 5,),
+            SizedBox(height: 20,),
             campoTexto("Email", Icons.email),
           ],
         ),
@@ -35,12 +38,11 @@ class _LoginPagesState extends State<LoginPages> {
   Widget campoTexto(String texto, IconData icono){
     return TextField(
       decoration: InputDecoration(
-        labelText: texto,
-        icon: Icon(icono),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10)
-        )
-      ),
+        fillColor: Color.fromRGBO(171, 171, 11, 0.098),
+                labelText: 'Nombre de la carrera',
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+                prefixIcon: const Icon(Icons.school, color: Color.fromRGBO(199, 26, 7, 1)),
+              ),
     );
   }
 
