bdddb41798e10290b753df612d3530e72358d042
rodriquelali <rodriquelali@gmail.com>
2025-08-04 08:31:09 -0400

    Corrección  de Task.

--- calculadora_proyect/lib/model/Calculadora.dart
+++ calculadora_proyect/lib/model/Calculadora.dart
@@ -6,11 +6,11 @@ class Calculadora {
   Calculadora(this._num1, this._num2);
 
 
-  //Crear una clase que que valide los datos de entrada, y llame al clase Calculadora.
-  //utlizar POO, asociacion, agregacion o dependencia. 
-  //y verificar procimpio.
-  // diseño ua UI de la calculadora
-  // UI solo tiene que llamar ala clase validar, no tiener ninguna operacion.
+  // Crear una clase que que valide los datos de entrada, y llame al clase Calculadora.
+  // utlizar POO, asociacion, agregacion o dependencia. 
+  // y verificar que principio es.
+  // rediseño La UI de la calculadora.
+  // UI solo tiene que llamar ala clase validar, no tiene que realizar ninguna operacion.
 
   //metodos
   String sumar() {
